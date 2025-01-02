7881d849401acbfc1233157a47ce662e4601fe42
peterhuang569 <peterzdhuang@gmail.com>
2025-01-01 20:51:26 -0700

    website calendar

--- app/controllers/spendings_controller.rb
+++ app/controllers/spendings_controller.rb
@@ -8,8 +8,40 @@ class SpendingsController < ApplicationController
       format.html # This is for regular HTML requests
       format.turbo_stream # This is for Turbo Stream updates
     end
+
+    # Get month and year from params or use the current date
+    @month = (params[:month] || Time.now.month).to_i
+    @year = (params[:year] || Time.now.year).to_i
+
+    # Compute first and last days of the requested month
+    first_day = Date.new(@year, @month, 1)
+    last_day = first_day.end_of_month
+
+    # Fetch spending records for the month
+    spending_records = Spending.where(created_at: first_day..last_day)
+
+    # Prepare daily totals
+    days_in_month = last_day.day
+    @daily_totals = Array.new(days_in_month, 0.0)
+
+    spending_records.each do |record|
+      day = record.created_at.day
+      @daily_totals[day - 1] += record.price
+    end
   end
 
+  # GET /daily
+  def daily
+    # Fetch all spending records for the specified date
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @spendings = Spending.where(date_time_utc: @date.beginning_of_day..@date.end_of_day)
+
+    respond_to do |format|
+      format.html # For regular HTML requests
+      format.turbo_stream # For Turbo Stream updates
+      format.json { render json: @spendings } # Optional JSON response for APIs
+    end
+  end
   # GET /spendings/:id
   def show
     render json: @spending
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "spendings#index"
+  get "/:date", to: "spendings#daily", constraints: { date: /\d{4}-\d{2}-\d{2}/ }, as: :daily_spending
   resources :spendings, only: [ :index, :show, :create, :update, :destroy ]
 end
